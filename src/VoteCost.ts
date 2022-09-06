65ab73cbc9a9d7c391f3e2119243b00b7b578e81
bijianing97 <826015751@qq.com>
2022-09-06 11:20:41 +0800

    chore: update startBlock

--- src/VoteCost.ts
+++ src/VoteCost.ts
@@ -16,7 +16,7 @@ export function handleStake(event: Stake): void {
       voterInfo = new VoterInfo(voterInfoID)
       voterInfo.cost = event.params.value
       voterInfo.timestamp = event.block.timestamp
-      voterInfo.commissionAddress = stakemanager.validators(event.params.validator).value1
+      voterInfo.commissionAddress = stakemanager.validators(event.params.validator).value1.toHex()
       const voterInfos = voter.voterInfo
       voterInfos.push(voterInfo.id)
       voter.voterInfo = voterInfos
@@ -28,7 +28,7 @@ export function handleStake(event: Stake): void {
     const voterInfo = new VoterInfo(voterInfoID)
     voterInfo.cost = event.params.value
     voterInfo.timestamp = event.block.timestamp
-    voterInfo.commissionAddress = stakemanager.validators(event.params.validator).value1
+    voterInfo.commissionAddress = stakemanager.validators(event.params.validator).value1.toHex()
     const voterInfos = [voterInfo.id]
     voter.voterInfo = voterInfos
     voter.timestamp = event.block.timestamp
@@ -46,13 +46,16 @@ export function handleStartUnstake(event: StartUnstake): void {
     return
   }
   const sharesAddress = voterInfo.commissionAddress
-  const shareContract = ERC20.bind(Address.fromString(sharesAddress.toString()))
+  const shareContract = ERC20.bind(Address.fromString(sharesAddress))
   const sharesAmount = shareContract.balanceOf(Address.fromString(voterID))
+  voterInfo.sharesAmount = sharesAmount
+
   if (sharesAmount.gt(BigInt.fromI32(0))) {
     const shareToRei = stakemanager.estimateSharesToAmount(event.params.validator, sharesAmount)
-    voterInfo.cost = voterInfo.cost.ge(shareToRei) ? sharesAmount : voterInfo.cost
+    voterInfo.balance = shareToRei
+    voterInfo.costAmout = voterInfo.cost.ge(shareToRei) ? sharesAmount : voterInfo.cost
   } else {
-    voterInfo.cost = BigInt.fromI32(0)
+    voterInfo.costAmout = BigInt.fromI32(0)
   }
   voterInfo.save()
 }
